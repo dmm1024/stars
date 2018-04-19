0489cf9b5f6fbaf397357ddae29ab7f2ec2bedd5
dmnmkua <dmn.mk.ua@gmail.com>
2018-04-19 23:42:56 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,54 @@
+const btn = document.querySelector('.start');
+
+//  array colors
+const colors = ['red', 'blue', 'green'];
+
+function createStar(count = 10) {
+  for(let i = 0; i < count; i++) {
+    let size = Math.floor(Math.random() * 50);
+    let posX = Math.floor(Math.random() * document.documentElement.clientWidth);
+    let posY = Math.floor(Math.random() * document.documentElement.clientHeight);
+    let color = colors[Math.floor(Math.random() * colors.length)];
+    let side = Math.round(Math.random());
+
+    let star = document.createElement('div');
+    star.classList.add('star');
+    star.style.width = `${size}px`;
+    star.style.height = `${size}px`;
+    star.style.top = `${posY}px`;
+    star.style.left = `${posX}px`;
+    star.style.background = `${color}`;
+    document.body.appendChild(star);
+
+    let sideInterval = setInterval(() => {
+      side++;
+
+      if(side % 2 === 0) {
+        posX = posX - 100;
+        star.style.left = `${posX}px`;
+      }
+      else {
+        posX = posX + 100;
+        star.style.left = `${posX}px`;
+      }
+    }, 1000);
+
+    let interval = setInterval(() => {
+      // let count = 0;
+      // count++;
+
+      if(posY > 0) {
+        posY--;
+        star.style.top = `${posY}px`;
+      }
+      else {
+        clearInterval(interval);
+        document.body.removeChild(star);
+      }
+    }, 10);
+  }
+}
+
+btn.addEventListener('click', () => {
+  createStar();
+})
